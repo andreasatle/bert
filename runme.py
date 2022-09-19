b60d6d1e5854ae8f06ee4477ab838cc3f8a8e14a
Andreas Atle <atle.andreas@gmail.com>
2022-09-19 07:47:54 -0500

    Avoid creating __pycache__

---
+++ runme.py
@@ -0,0 +1,11 @@
+'''
+This module avoids creating __pycache__ when importing modules, and runs the workflow.
+'''
+
+if __name__ == "__main__":
+    import sys
+
+    sys.dont_write_bytecode = True
+    import workflow
+
+    workflow.run(1)
